7567032811a184e5329a8a2c48e5b7c206bc17eb
LordHob <rafaelginergomez@gmail.com>
2021-12-22 16:55:57 +0100

    up

--- backendlaravel/app/Models/User.php
+++ backendlaravel/app/Models/User.php
@@ -4,14 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
+    use HasFactory;
 
     protected $fillable = [
         'email', 'userName', 'password', 'role', 'steamUserName'
--- backendlaravel/routes/api.php
+++ backendlaravel/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PartyController;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\MensajeController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +18,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/usuario', function (Request $request) {
+//     return $request->usuario();
+// });
+//Registro
+Route::post('newUser', [AuthController::class, "userRegister"]);
+//login
+Route::post('loginUser', [AuthController::class, "userLogin"]);
+
+//usuarios
+Route::get('Usuario', [UserController::class, "showAllUsuario"]);
+Route::post('Usuario', [UserController::class, "addUsuarios"]);
+Route::get('Usuario/{id}', [UserController::class, "UsuariosByID"]);
+Route::put('Usuario/{id}', [UserController::class, "UpdateUsuarios"]);
+Route::delete('Usuario/{id}', [UserController::class, "DeleteUsuarios"]);
+//Partys
+Route::post('Party', [PartyController::class, "nuevaparty"]);
+Route::get('Party', [PartyController::class, "showAllparty"]); //
+Route::get('party/{id}', [PartyController::class, "showpartyByID"]);
+Route::delete('Party/{id}', [PartyController::class, "Deleteparty"]);
+Route::put('Party/{id}', [PartyController::class, "Updateteparty"]);
+// Game
+Route::post('Game', [GameController::class, "createGame"]); //
+Route::get('Game', [GameController::class, "showAllGame"]); //
+Route::get('Game/{id}', [GameController::class, "showGameByID"]);
+Route::put('Game/{id}', [GameController::class, "updateGame"]);
+Route::delete('Game/{id}', [GameController::class, "deleteGame"]);
+//Mensajes
+Route::post('Mensaje', [MensajeController::class, "createMensaje"]);
+Route::delete('Mensaje/{id}', [MensajeController::class, "deleteMensaje"]);
+Route::get('Mensaje/{id}', [MensajeController::class, "MensajebyID"]);
+Route::get('Mensaje', [MensajeController::class, "showAllMensaje"]);;
